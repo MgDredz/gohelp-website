21ff218e2dfba90d51f09f0c20afc6b3d51c312d
MgDredz <fabiiomiguel8@gmail.com>
2024-05-08 21:15:33 +0100

    forgor

--- back/startbootstrap-sb-admin-2-gh-pages/js/criar-PlanoInciativa.js
+++ back/startbootstrap-sb-admin-2-gh-pages/js/criar-PlanoInciativa.js
@@ -120,7 +120,7 @@ document.addEventListener('DOMContentLoaded', () => {
         today.setHours(0, 0, 0, 0); // Reset time to the start of the day for accurate comparison
         selectedDate.setHours(0, 0, 0, 0); // Ensure time is set to start of the day
 
-        if (!titulo || !type || !localidade || !region || !data || !horaInicio || !horaFim || !participantes|| !descricao || !imagemFile) {
+        if (!titulo || !type || !localidade || !region || !data || !horaInicio || !horaFim || !participantes|| !descricao || gestor || !imagemFile) {
             errorMessage.classList.remove('d-none');
             return;
         }
